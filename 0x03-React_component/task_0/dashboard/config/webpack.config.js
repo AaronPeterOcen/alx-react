648280261a4e3a2ec6867e8cf424f297f536aca4
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-05-30 20:57:30 +0300

    updated task 0

--- 0x03-React_component/task_0/dashboard/config/webpack.config.js
+++ 0x03-React_component/task_0/dashboard/config/webpack.config.js
@@ -1,54 +1,52 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        // type: 'asset/resource',
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
+export const entry = "./src/index.js";
+export const output = {
+  filename: "bundle.js",
+};
+export const mode = "development";
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader"],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      // type: 'asset/resource',
+      use: [
+        "file-loader",
+        {
+          loader: "image-webpack-loader",
+          options: {
+            bypassOnDebug: true, // webpack@1.x
+            disable: true, // webpack@2.x and newer
           },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-  devServer: {
-    static: "./dist",
-    compress: true,
-    open: true,
-    hot: true,
-    port: 8564,
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      name: "index.html",
-      inject: false,
-      template: "./dist/index.html",
-    }),
+        },
+      ],
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+    },
   ],
 };
+export const resolve = {
+  extensions: ["*", ".js", ".jsx"],
+};
+export const devServer = {
+  static: "./dist",
+  compress: true,
+  open: true,
+  hot: true,
+  port: 8564,
+};
+export const devtool = "inline-source-map";
+export const plugins = [
+  new HtmlWebpackPlugin({
+    name: "index.html",
+    inject: false,
+    template: "./dist/index.html",
+  }),
+];
