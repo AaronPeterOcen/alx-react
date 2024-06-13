2a5430c2915db7b21d8e83eaf638976c9d3f78b2
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-13 10:57:29 +0300

    function to return value of the object at the defined path

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,1 +1,7 @@
-import { fromJS } from "./node_modules/immutable/dist/immutable";
+import { fromJS } from './node_modules/immutable/dist/immutable';
+
+export default function accessImmutableObject (object, array) {
+  const mapObj = fromJS(object);
+
+  return mapObj.getIn(array, undefined);
+}
