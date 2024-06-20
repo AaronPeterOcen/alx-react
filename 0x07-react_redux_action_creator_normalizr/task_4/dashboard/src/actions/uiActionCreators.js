c9251737ae5a205ec8cee70fa9dd8cdcf8c6e71c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-06-20 09:12:44 +0300

    creating the action creator

--- 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/uiActionCreators.js
@@ -1,5 +1,23 @@
-import React from "react";
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+} from "./uiActionTypes";
 
-export default function uiActionCreators() {
-  return <div>uiActionCreators</div>;
+export function login(email, password) {
+  return {
+    type: LOGIN,
+    user: { email, password },
+  };
 }
+
+export const logout = () => ({ type: LOGOUT });
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER,
+});
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER,
+});
